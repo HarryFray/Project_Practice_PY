91f7e72cc0a18c6c81cea1e983bfcefd06c9ef52
HarryFray <harry.fray7@gmail.com>
2017-11-18 15:49:52 -0800

    studied documentaion for bs4 and requests

--- DataScrapePractice.py
+++ DataScrapePractice.py
@@ -2,13 +2,35 @@ from bs4 import BeautifulSoup
 import requests
 
 
+# APIkey = 2a016a417daf4448aca7554281008a52
+url = 'https://en.wikipedia.org/wiki/Gu%C3%A9ridon'
+''' gets content'''
+x = requests.get(url)
 
-x = requests.get('https://www.wikipedia.org/')
+
+'''beatifulsoup makes the content pulled usable '''
+soup = BeautifulSoup(x.content,'html.parser')
 
 
-''' .content gives all info pulled from web
-beatifulsoup makes that content pulled useable '''
-soup = BeautifulSoup(x.content)
+''' study documentation of requests '''
+links = soup.find_all("a")
+for link in links:
+    print link.get('href')
+
+'''returns all text'''
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
